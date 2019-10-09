dede1f53b8fda895a997f463dbd30c7bb82edcfd
Kyler Kershaw <KershawK5646@student.faytechcc.edu>
2019-10-09 14:43:33 -0400

    Getting closer

--- house.py
+++ house.py
@@ -6,14 +6,21 @@ from room import Room
 
 class House:
     """ a House contains multiple Room Objects """
-    # Create rooms
-    start = Room(0, "The Void","You should have never seen this room...")
-    kitchen = Room(1, "Kitchen","There is rotting food and dirty dished piled high.")
-    bedroom = Room(2, "Bedroom","A king size bed lays on the far wall. ")
-    livingroom = Room(3, "Livingroom","Everything is caked in dust.")
-    bathroom = Room(4, "Bathroom","Mold everywhere...")
-    garage = Room(5, "Garage","There are oil stains splattered on the concrete...")
-    
-    self.rooms = [start, kitchen, bedroom, livingroom, bathroom, garage]
-    
-    
+    def __init__(self):
+        # Create rooms
+        start = Room(0, "The Void","You should have never seen this room...")
+        kitchen = Room(1, "Kitchen","There is rotting food and dirty dished piled high.")
+        bedroom = Room(2, "Bedroom","A king size bed lays on the far wall. ")
+        livingroom = Room(3, "Livingroom","Everything is caked in dust.")
+        bathroom = Room(4, "Bathroom","Mold everywhere...")
+        garage = Room(5, "Garage","There are oil stains splattered on the concrete...")
+        
+        self.rooms = [start, kitchen, bedroom, livingroom, bathroom, garage]
+        
+def main():
+    house = House()
+    for room in house.rooms:
+        print(room)
+
+if __name__ == "__main__":
+    main()
--- room.py
+++ room.py
@@ -5,16 +5,20 @@ The room.py
 
 class Room:
     """The room object"""
-    def __init__ (self, roommId, name, description):
+    def __init__ (self, roomId, name, description):
         """Create a room object"""
         self.roomId = roomId
         self.name = name
         self.description = description
         
+        self.exits = {} # empty dictionary
+        
     def __str__ (self):
         """Create a human readable version"""
         output = "Name: " + self.name
-        output += "\n" + "Description: " + self.description
+        output += "\n"
+        output += "Description: " + self.description
+        output += "Exits: " + str(self.exits)
         return output
     
     
